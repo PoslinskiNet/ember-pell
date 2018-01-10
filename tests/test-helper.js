c5d7dc705da067c0c38c16428da4b5c84d6387fa
Dawid Pośliński <dposlinski@gmail.com>
2018-01-10 21:05:25 +0100

    Ember 2.18 upgrade

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,4 +1,7 @@
-import resolver from './helpers/resolver';
-import { setResolver } from 'ember-mocha';
+import Application from '../app';
+import { setApplication } from '@ember/test-helpers';
+import { start } from 'ember-qunit';
 
-setResolver(resolver);
+setApplication(Application.create({ autoboot: false }));
+
+start();
