4802c8642c535e99b4ebde7dbb67679b435beba1
Dawid Pośliński <dposlinski@gmail.com>
2018-12-28 13:06:47 +0100

    Minnor package upgrades

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,50 +2,50 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ["ember"],
+  extends: ["eslint:recommended", "plugin:ember/recommended"],
   env: {
     browser: true
   },
   rules: {
+    "ember/no-observers": "off"
   },
   overrides: [
     // node files
     {
       files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'index.js',
-        'testem.js',
-        'config/**/*.js',
-        'tests/dummy/config/**/*.js'
+        ".eslintrc.js",
+        ".template-lintrc.js",
+        "ember-cli-build.js",
+        "index.js",
+        "testem.js",
+        "config/**/*.js",
+        "tests/dummy/config/**/*.js"
       ],
       excludedFiles: [
-        'addon/**',
-        'addon-test-support/**',
-        'app/**',
-        'tests/dummy/app/**'
+        "addon/**",
+        "addon-test-support/**",
+        "app/**",
+        "tests/dummy/app/**"
       ],
       parserOptions: {
-        sourceType: 'script',
+        sourceType: "script",
         ecmaVersion: 2015
       },
       env: {
         browser: false,
         node: true
       },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        'node/no-extraneous-require': 'off'
-      })
+      plugins: ["node"],
+      rules: Object.assign(
+        {},
+        require("eslint-plugin-node").configs.recommended.rules,
+        {
+          "node/no-extraneous-require": "off"
+        }
+      )
     }
   ]
 };
